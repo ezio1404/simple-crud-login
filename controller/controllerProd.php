36000ac0edb9ffed18005e003517d72ed51c7a0c
ezio1404 <zuming1404@gmail.com>
2018-05-30 03:27:26 +0800

    Done

--- controller/controllerProd.php
+++ controller/controllerProd.php
@@ -5,16 +5,6 @@ if(isset($_POST['addProd'])){
     $name=htmlentities($_POST['name']);
     $desc=htmlentities($_POST['desc']);
     $price=htmlentities($_POST['price']);
-    // $test = $_POST;
-    // foreach($test as $key){
-    //     if($key!="")
-    //     $flag=true;
-    // }
-
-    // $name="nails";
-    // $desc="1/2 inches";
-    // $price=10.50;
-
     $data=array($name,$desc,$price);
     insertProd($data);
     header("Location:../view/product.php");
@@ -25,13 +15,6 @@ if(isset($_POST['updateProd'])){
     $desc=htmlentities($_POST['desc']);
     $price=htmlentities($_POST['price']);
     $status=htmlentities($_PPOST['status']);
-
-    // $id=1;
-    // $name="nails";
-    // $desc="1/2 inches";
-    // $price=5.50;
-    // $status=1;
-
     $data=array($name,$desc,$price,$status,$id);
     updateProd($data);
     header("Location:../view/product.php");
